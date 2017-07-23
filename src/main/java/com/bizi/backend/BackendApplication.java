39a7bb67993ec1f163262567bd01920131f8305d
bishion <bishion@163.com>
2017-07-23 19:03:49 +0800

    index学习中

---
+++ src/main/java/com/bizi/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.bizi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by guofangbi on 2017/7/23.
+ */
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class,args);
+    }
+}
